228ca8fa3e5ca3fe8e1c5b4d86dc500305158fa9
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2022-05-13 08:25:12 +0200

    Add files via upload

---
+++ custom_files/custom_app_review.rb
@@ -0,0 +1,31 @@
+module AresMUSH
+  module Chargen  
+    
+    def self.custom_app_review(char)
+            
+      # If you don't want to have any custom app review steps, return nil
+      # return nil
+            
+      # Otherwise, return a message to display.  Here's an example of how to 
+      # give an alert if the character has chosen an invalid position for their 
+      # faction.
+      #
+      
+      msg = ""
+      
+      rank = char.ranks_rank
+      if ((rank == "Adept") && (char.marque == ""))
+         msg = "%xrAdept concepts need to set the starting percentage of their marque! %xn"
+      end
+             
+      if (msg == "")
+         msg = t('chargen.ok')
+      end
+
+      return Chargen.format_review_status "Checking custom settings.", msg
+      #
+      # You can also use other built-in chargen status messages, like t('chargen.not_set').  
+      # See https://www.aresmush.com/tutorials/config/chargen.html for details.
+    end
+  end
+end
---
+++ custom_files/custom_char_fields.rb
@@ -0,0 +1,29 @@
+module AresMUSH
+  module Profile
+    class CustomCharFields
+      
+      def self.get_fields_for_viewing(char, viewer)
+        return { marque: char.marque.to_i }
+      end
+    
+      def self.get_fields_for_editing(char, viewer)
+       return {}
+       end
+
+      def self.get_fields_for_chargen(char)
+         return { marque: Website.format_input_for_html(char.marque),
+           cg_adept: char.ranks_rank == "Adept" }
+      end
+      
+      def self.save_fields_from_profile_edit(char, char_data)
+        return {}
+      end
+      
+      def self.save_fields_from_chargen(char, chargen_data)
+        char.update(marque: chargen_data[:custom][:marque])
+        return []
+      end
+      
+    end
+  end
+end
