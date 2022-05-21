91dd71f425e5fe1cd7ea163496103fd481e2087e
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2022-05-21 16:19:50 +0200

    Delete custom-routes.js

--- webportal/custom-routes.js
+++
@@ -1,7 +0,0 @@
-export default function setupCustomRoutes(router) {
-  // Define your own custom routes here, just as you would in router.js but using 'router' instead of 'this'.
-  // For example:
-  // router.route('yourroute');
- router.route('courtesan-management');
- router.route('courtesan-management-house', { path: '/courtesan-management/:house' });
-}
