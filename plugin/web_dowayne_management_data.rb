f0c05e8397eb012cfb611fe0e10452a32ab5cc43
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2022-09-15 15:44:58 +0200

    Update web_dowayne_management_data.rb

--- plugin/web_dowayne_management_data.rb
+++ plugin/web_dowayne_management_data.rb
@@ -2,8 +2,8 @@ module AresMUSH
   module Marque
     class DowayneManagementDataBuilder
       def build(char, viewer)
-        has_permission = (char == viewer) || (viewer.is_admin?)
-        if (char.ranks_rank != "Dowayne") || !(has_permission) 
+        has_permission = (char == viewer) || (viewer && (viewer.is_admin?))
+        if !(char.has_permission?("manage_marques")) || !(has_permission)
            return nil
         end
 
